c77ac75a8bbdf7f2e3bbbd8e330299e95078f9ff
Tyler Kass <102496525+TylerKass@users.noreply.github.com>
2023-09-07 09:25:58 -0700

    Update Hourly.java

--- Hourly.java
+++ Hourly.java
@@ -1,7 +1,5 @@
 /*
-CSC 205: # 11210 / M 5:20 - 7:00 PM
-Program #1
-Author: Tyler Kass 36910537
+Author: Tyler Kass
 Description: We have three abstract methods that we need to implement.
  */
 public interface Hourly {
