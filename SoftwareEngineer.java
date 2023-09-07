9af8976cfc9bc6384a0e446d18d8dc70d6a16462
Tyler Kass <102496525+TylerKass@users.noreply.github.com>
2023-09-07 09:25:48 -0700

    Update SoftwareEngineer.java

--- SoftwareEngineer.java
+++ SoftwareEngineer.java
@@ -1,7 +1,5 @@
 /*
-CSC 205: # 11210 / M 5:20 - 7:00 PM
-Program #1
-Author: Tyler Kass 36910537
+Author: Tyler Kass
 Description: This class, extends employee with a parameterized constructor that stores, name and salary.
 We also have a giveRaise method, that gives the software engineer employees a raise.
  */
